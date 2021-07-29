59f1a2aaed3e432e364b2657d38a4f5a35a421b8
Sharon Grossman <sharong1337@gmail.com>
2021-07-29 13:52:56 +0300

    pass registry

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Counter, Histogram, register } from 'prom-client';
+import { Counter, Histogram, Registry } from 'prom-client';
 import type { Unpromisify, MonitorOptions } from './types';
 import logger from './logger';
 
@@ -6,27 +6,60 @@ const histograms: Record<string, Histogram<string>> = {};
 
 const counters: Record<string, Counter<string>> = {};
 
-const createHistogram = ({ name, help, labelNames }: { name: string; help: string; labelNames?: string[] }) => {
+const createHistogram = ({
+  name,
+  help,
+  registry,
+  labelNames,
+}: {
+  name: string;
+  help: string;
+  registry: Registry;
+  labelNames?: string[];
+}) => {
   if (histograms[name]) return histograms[name];
 
-  histograms[name] = new Histogram({ name, help, buckets: [0.25, 0.5, 0.9, 0.99], labelNames, registers: [register] });
+  const histogram = new Histogram({ name, help, buckets: [0.25, 0.5, 0.9, 0.99], labelNames });
 
-  return histograms[name];
+  histograms[name] = histogram;
+  registry.registerMetric(histogram);
+
+  return histogram;
 };
 
-const createCounter = ({ name, help, labelNames }: { name: string; help: string; labelNames?: string[] }) => {
+const createCounter = ({
+  name,
+  help,
+  registry,
+  labelNames,
+}: {
+  name: string;
+  help: string;
+  registry: Registry;
+  labelNames?: string[];
+}) => {
   if (counters[name]) return counters[name];
-  counters[name] = new Counter({ name, help, labelNames, registers: [register] });
 
-  return counters[name];
+  const counter = new Counter({ name, help, labelNames });
+
+  counters[name] = counter;
+  registry.registerMetric(counter);
+
+  return counter;
 };
 
-const monitor = <T>(scope: string, method: string, callable: () => T, options?: MonitorOptions) => {
-  const counter = createCounter({ name: `${scope}_count`, help: `${scope}_count`, labelNames: ['method', 'result'] });
+const monitor = <T>(scope: string, method: string, callable: () => T, registry: Registry, options?: MonitorOptions) => {
+  const counter = createCounter({
+    name: `${scope}_count`,
+    help: `${scope}_count`,
+    registry,
+    labelNames: ['method', 'result'],
+  });
   const histogram = createHistogram({
     name: `${scope}_execution_time`,
     help: `${scope}_execution_time`,
     labelNames: ['method', 'result'],
+    registry,
   });
 
   const stopTimer = histogram.startTimer();
@@ -66,6 +99,6 @@ const monitor = <T>(scope: string, method: string, callable: () => T, options?: 
   }
 };
 
-export default (scope = 'monitor') =>
+export default (scope = 'monitor', registry: Registry) =>
   <T>(method: string, callable: () => T, options?: MonitorOptions) =>
-    monitor(scope, method, callable, options);
+    monitor(scope, method, callable, registry, options);
